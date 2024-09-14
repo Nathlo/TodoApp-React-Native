6daf9d19bfc64c12fd7878c9128bea154f3b9511
Nathlo <thyalan.aurlence@gmail.com>
2024-09-14 11:11:19 -0400

    Display tasks list from API JSONPlaceholder

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
-import { Text, SafeAreaView, FlatList } from 'react-native'
-import React, { useState } from 'react'
-import { Button, TextInput } from 'react-native-paper'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, SafeAreaView, FlatList, View } from 'react-native'
+import { ActivityIndicator, Text, Button, TextInput, List, Appbar, FAB } from 'react-native-paper'
 import { styles } from './style'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
 const App = () => {
-  const [task, setTask] = useState('');
-  const [tasks, setTasks] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const addTask = () => {
-    if (task.length > 0) {
-      setTasks([...tasks, { id: tasks.length + 1, name: task }]);
-      setTask('');
-    }
+// Fonction pour récupérer les données depuis l'API JSOPlaceholder
+const fetchTodos = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    setTodos(data);
+    setLoading(false);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données', error);
+    setLoading(false);
   }
+};
 
-  const deleteTask = (id) => {
-    setTasks(tasks.filter( (task) => task.id !== id));
-  }
+// Utiliser UseEffect pour récupérer les données lors du montage du composant
+useEffect( () => {
+  fetchTodos();
+}, []);
+
+if (loading) {
+  return (
+    <View style={styles.loadingContaier}>
+      <ActivityIndicator animating={true} size="large" />
+    </View>
+  );
+}
+
 
   return (
 
@@ -31,18 +48,20 @@ const App = () => {
       <TextInput  
         label="Nouvelle tâche"
         value={task}
+        onChangeText={ (text) => setTask(text)}
         mode="outlined"
         style={styles.input}
       />
 
       <Button 
         mode="contained"
+        onPress={addTask}
         style={styles.addButtton}>
           Ajoutez une tâche
       </Button>
 
       <FlatList 
-        data={task}
+        data={tasks}
         keyExtractor={ (item) => item.id.toString() }
         renderItem={ ({item}) => (
           <List.Item  
